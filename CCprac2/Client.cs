4feb09da707c71941836cdfffa3e84fb08869312
Aron List <aronotmar@live.nl>
2014-10-03 21:29:23 +0200

    we can now connect send and disconnect, not routing though

---
+++ CCprac2/Client.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetwProg
+{
+    class Client
+    {
+        public int RemotePort { get; private set; }
+        TcpClient socket;
+        StreamReader clientIn;
+        StreamWriter clientOut;
+
+        bool keepAlive = true;
+
+        public delegate void MessageRecievedHandler(string message, int remotePort);
+
+        public event MessageRecievedHandler MessageRecieved;
+
+        public delegate void ConnectionCLosedHandler(int remotePort);
+
+        public event ConnectionCLosedHandler ConnectionClosed;
+
+        public Client(TcpClient socket)
+        {
+            this.socket = socket;
+            clientIn = new StreamReader(socket.GetStream());
+            clientOut = new StreamWriter(socket.GetStream());
+
+            clientOut.AutoFlush = true;
+
+            clientOut.WriteLine(Program.myPort);
+
+            RemotePort = int.Parse(clientIn.ReadLine());
+
+            Console.WriteLine("//Connected to: " + RemotePort);
+
+            Thread t = new Thread(ReadMessages);
+            t.Start();
+        }
+
+        public void SendMessage(string message)
+        {
+            clientOut.WriteLine(message);
+        }
+
+        public enum DisconnectReason
+        {
+            Command,
+            Message,
+        }
+
+        public void Disconnect(DisconnectReason reason)
+        {
+            keepAlive = false;
+            if (reason == DisconnectReason.Command) SendMessage("closing");
+            socket.Close();
+            clientOut.Dispose();
+            clientIn.Dispose();
+            if (ConnectionClosed != null)
+                ConnectionClosed(RemotePort);
+            Console.WriteLine("//Disconnected from: " + RemotePort + " | Reason: " + reason);
+        }
+
+        void ReadMessages()
+        {
+            while (keepAlive)
+            {
+                try
+                {
+                    string s = clientIn.ReadLine();
+                    if (s == "closing")
+                        Disconnect(DisconnectReason.Message);
+                    else
+                        MessageRecieved(s, RemotePort);
+                }
+                catch { }
+            }
+        }
+
+    }
+}
--- CCprac2/Program.cs
+++ CCprac2/Program.cs
@@ -2,18 +2,138 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
 
 namespace NetwProg
 {
     class Program
     {
+        public static int myPort;
+        int[] myNeighbors;
+        public Dictionary<int, Client> connections;
         static void Main(string[] args)
         {
+            Program p = new Program(args);
+        }
+
+        public Program(string[] input)
+        {
+            myPort = int.Parse(input[0]);
+            myNeighbors = new int[input.Length - 1];
+            for (int i = 1; i < input.Length; i++)
+            {
+                myNeighbors[i-1] = int.Parse(input[i]);
+            }
+
+            connections = new Dictionary<int, Client>();
+
+            Console.Title = "Netchange " + myPort;          
+
+            Listener listener = new Listener();
+            Thread t = new Thread(listener.Listen);
+            t.Start(this);
+
+            for (int i = 0; i < myNeighbors.Length; i++)
+            {
+                if (myNeighbors[i] > myPort)
+                {
+                    TcpClient c = new TcpClient("localhost", myNeighbors[i]);
+                    AddClient(c);
+                }
+            }
+
+            while (true)
+            {
+                string[] s = Console.ReadLine().Split();
+                if (s[0] == "B")
+                {
+                    int remotePort = int.Parse(s[1]);
+                    connections[remotePort].SendMessage(s[2]);
+                }
+                else if (s[0] == "D")
+                {
+                    int remotePort = int.Parse(s[1]);
+                    if (connections.ContainsKey(remotePort))
+                        connections[remotePort].Disconnect(Client.DisconnectReason.Command);
+                }
+                else if (s[0] == "C")
+                {
+                    int remotePort = int.Parse(s[1]);
+                    TcpClient c = new TcpClient("localhost", remotePort);
+                    AddClient(c);
+                }
+                switch (s[0])
+                {
+                    case "B":
+                        int remotePort = int.Parse(s[1]);
+                        connections[int.Parse(s[1])].SendMessage(s[2]);
+                        break;
+                    case "D":
+                        int remotePort = int.Parse(s[1]);
+                        if (connections.ContainsKey())
+                        connections[int.Parse(s[1])].Disconnect(Client.DisconnectReason.Command);
+                        break;
+                    case "C":
+                        int remotePort = int.Parse(s[1]);
+                        TcpClient c = new TcpClient("localhost", remotePort);
+                        AddClient(c);
+                        break;
+                }
+            }
+            
+
+            t.Join();
+        }
+
+        public void AddClient(TcpClient temp)
+        {
+            Client client = new Client(temp);
+            client.MessageRecieved += HandleMessage;
+            client.ConnectionClosed += ConnectionClosed;
+            connections.Add(client.RemotePort, client);
+        }
+
+        public void HandleMessage(string message, int remotePort)
+        {
+            Console.WriteLine("//Message recieved from: " + remotePort);
+            Console.WriteLine(message);
+        }
+
+        public void ConnectionClosed(int remotePort)
+        {
+            connections.Remove(remotePort);
         }
     }
 
+    class Listener
+    {
+        TcpListener server;
+        public void Listen(object p)
+        {
+            server = new TcpListener(IPAddress.Any, Program.myPort);
+            server.Start();
+            Console.WriteLine("//listening on port " + Program.myPort);
+            try
+            {
+                while (true)
+                {
+                    TcpClient temp = server.AcceptTcpClient();               
+                    Program pr = (p as Program);
+                    pr.AddClient(temp);
+                }
+            }
+            catch { }
+
+        }
+
+        public void Stop()
+        {
+            server.Stop();
+        }
+    }
 
 }
